9759089f23fd6315f0eb0b49266cdc8737480cc4
Francis Fulgencio <francisfulgencio@gmail.com>
2019-06-29 12:17:14 -0400

    se modifico el archivo app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ConactComponent } from './conact/conact.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ConactComponent
+    ConactComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
